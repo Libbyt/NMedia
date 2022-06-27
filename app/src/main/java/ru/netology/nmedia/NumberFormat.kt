4199b42f5f3c22a351ba93b400f8b449820df314
Daniel <ortixon@gmail.com>
2022-06-27 16:06:45 +0500

    Like and Share onClick Actions

---
+++ app/src/main/java/ru/netology/nmedia/NumberFormat.kt
@@ -0,0 +1,14 @@
+package ru.netology.nmedia
+
+import android.icu.number.Notation
+import android.icu.number.NumberFormatter
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.math.RoundingMode
+import java.util.*
+
+@RequiresApi(Build.VERSION_CODES.R)
+fun numberFormat(number: Int): String? {
+    return NumberFormatter.with().roundingMode(RoundingMode.FLOOR)
+        .notation(Notation.compactShort()).locale(Locale.getDefault()).format(number).toString()
+}
